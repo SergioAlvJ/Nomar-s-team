d7d9b874ce28d4be0ed202f4441ca80d9f38e63c
SergioAlvJ <84820143+SergioAlvJ@users.noreply.github.com>
2025-02-14 10:48:41 -0700

    checo
    Checo parte

---
+++ Scrip.js
@@ -0,0 +1,12 @@
+
+/* Responsividad */
+@media (max-width: 768px) {
+    #Text_inicial {
+        text-align: center;
+        font-size: 1.8em;
+    }
+    .container {
+        flex-direction: column;
+        align-items: center;
+    }
+}
